343670b5189e0fe9ddc0bd4a5461def757a31e17
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-03-29 22:27:54 -0500

    Create 3Rr0r404a.py

---
+++ challenges/codeabbey/015/3Rr0r404a.py
@@ -0,0 +1,11 @@
+You have recommended this code as GOOD!
+def maxmin(data):
+    temp = data.split(" ");
+    mini = 9999999999999999999999999999999999999999999;
+    maxi = -999999999999999999999999999999999999999999;
+    for i in range(0,len(temp)):
+        if(int(temp[i]) < mini):
+            mini = int(temp[i]);
+        if(int(temp[i]) > maxi):
+            maxi = int(temp[i]);
+    print(str(maxi) + " " + str(mini));
