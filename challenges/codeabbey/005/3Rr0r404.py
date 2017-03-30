2bb4bf01d5ab837021eef59f2d3fc57aaf091f4c
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-03-29 22:17:17 -0500

    Create 3Rr0r404.py

---
+++ challenges/codeabbey/005/3Rr0r404.py
@@ -0,0 +1,17 @@
+def menorTriple(data):
+    t = data.split("\n");
+    temp = [];
+    res = "";
+    for i in range(0,int(t[0])):
+        temp = t[i+1].split(" ");
+        if (int(temp[0]) < int(temp[1])):
+            if (int(temp[0]) < int(temp[2])):
+                res += temp[0] + " ";
+            else:
+                res += temp[2] + " ";
+        elif (int(temp[1]) < int(temp[0])):
+            if (int(temp[1]) < int(temp[2])):
+                res += temp[1] + " ";
+            else:
+                res += temp[2] + " ";
+    print (res);
