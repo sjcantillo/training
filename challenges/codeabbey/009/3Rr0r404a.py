13598ce878cbd869f23b300a161061665f58f2db
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-03-29 22:22:04 -0500

    Create 3Rr0r404a.py

---
+++ challenges/codeabbey/009/3Rr0r404a.py
@@ -0,0 +1,12 @@
+def triangles(data):
+        temp = data.split("\n");
+        res = "";
+        t = [];
+        for i in range(0, int(temp[0])):
+                t = temp[i+1].split(" ");
+                
+                if (((int(t[0])+int(t[1]))>int(t[2])) and ((int(t[0])+int(t[2]))>int(t[1])) and ((int(t[1])+int(t[2]))>int(t[0]))):
+                        res += "1 ";
+                else:
+                        res += "0 ";
+        print(res);
