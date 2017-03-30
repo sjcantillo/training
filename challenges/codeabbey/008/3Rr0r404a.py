9c773ea60ef0fc9adfff6966d86336962e643a9c
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-03-29 22:21:05 -0500

    Create 3Rr0r404a.py

---
+++ challenges/codeabbey/008/3Rr0r404a.py
@@ -0,0 +1,12 @@
+def arithmeticProgression(data):
+        temp = data.split("\n");
+        res = "";
+        t = [];
+        n = 0;
+        for i in range(0,int(temp[0])):
+                t = temp[i+1].split(" ");
+                for x in range(0,int(t[2])):
+                        n += int(t[0]) + (x * int(t[1]));
+                res += str(n) + " ";
+                n=0;
+        print(res);
