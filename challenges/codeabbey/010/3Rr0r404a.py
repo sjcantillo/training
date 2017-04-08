da3b290d3cd70627602f4d4be64451b164a99ff8
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-04-08 12:57:03 -0500

    Update 3Rr0r404a.py

--- challenges/codeabbey/010/3Rr0r404a.py
+++ challenges/codeabbey/010/3Rr0r404a.py
@@ -1,12 +1,12 @@
 def linearFunction(data):
-        temp = data.split("\n");
-        t = [];
-        a = 0;
-        b = 0;
-        res = "";
-        for i in range(0,int(temp[0])):
-                t = temp[i+1].split(" ");
-                a = (int(t[3]) - int(t[1])) / (int(t[2]) - int(t[0]));
-                b = int(t[1]) - (a * int(t[0]));
-                res += "(" + str(int(a)) + " " + str(int(b)) + ") ";
-        print(res);
+  temp = data.split("\n");
+  t = [];
+  a = 0;
+  b = 0;
+  res = "";
+  for i in range(0,int(temp[0])):
+    t = temp[i+1].split(" ");
+    a = (int(t[3]) - int(t[1])) / (int(t[2]) - int(t[0]));
+    b = int(t[1]) - (a * int(t[0]));
+    res += "(" + str(int(a)) + " " + str(int(b)) + ") ";
+  print(res);
