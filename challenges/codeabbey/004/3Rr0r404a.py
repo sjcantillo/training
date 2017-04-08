c3c86a328c2292970aa1c270fafbf15195b086b6
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-04-08 13:04:39 -0500

    Update 3Rr0r404a.py

--- challenges/codeabbey/004/3Rr0r404a.py
+++ challenges/codeabbey/004/3Rr0r404a.py
@@ -1,11 +1,11 @@
 def menor(data):
-    t = data.split("\n");
-    temp = [];
-    res = "";
-    for i in range(0,int(t[0])):
-        temp = t[i+1].split(" ");
-        if (int(temp[0]) < int(temp[1])):
-            res += temp[0] + " ";
-        else:
-            res += temp[1] + " ";
-    print (res);
+  t = data.split("\n");
+  temp = [];
+  res = "";
+  for i in range(0,int(t[0])):
+    temp = t[i+1].split(" ");
+    if (int(temp[0]) < int(temp[1])):
+      res += temp[0] + " ";
+    else:
+      res += temp[1] + " ";
+  print (res);
