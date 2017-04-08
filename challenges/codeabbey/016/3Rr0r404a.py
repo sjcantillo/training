bc5a5fe69dd7c10ed606b344eb467e3f47523255
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-04-08 12:41:44 -0500

    Update 3Rr0r404a.py

--- challenges/codeabbey/016/3Rr0r404a.py
+++ challenges/codeabbey/016/3Rr0r404a.py
@@ -1,13 +1,13 @@
 def avg(data):
-        temp = data.split("\n");
-        res = "";
-        t=[];
-        n=0;
-        for i in range(0,int(temp[0])):
-                t = temp[i+1].split(" ");
-                for x in t:
-                        n += int(x);
-                n = round(n / (len(t)-1),0);
-                res += str(int(n)) + " ";
-                n = 0;
-        print(res);
+  temp = data.split("\n");
+  res = "";
+  t=[];
+  n=0;
+  for i in range(0,int(temp[0])):
+    t = temp[i+1].split(" ");
+    for x in t:
+      n += int(x);
+    n = round(n / (len(t)-1),0);
+    res += str(int(n)) + " ";
+    n = 0;
+  print(res);
