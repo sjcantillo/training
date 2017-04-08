e3dc9748916e63287dd6c26c61814ccf307049b1
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-04-08 11:57:27 -0500

    Update 3Rr0r404a.py

--- challenges/codeabbey/028/3Rr0r404a.py
+++ challenges/codeabbey/028/3Rr0r404a.py
@@ -1,17 +1,17 @@
 def BMI(data):
-    temp = data.split("\n");
-    res="";
-    t=[];
-    bmi = 0
-    for i in range(0,int(temp[0])):
-        t = temp[i+1].split(" ");
-        bmi = float(t[0]) / pow(float(t[1]),2);
-        if (bmi < 18.5):
-                res += "under ";
-        elif ( bmi >= 18.5 and bmi < 25.0):
-                res += "normal ";
-        elif ( bmi >=25.0 and bmi < 30.0):
-                res += "over ";
-        else:
-                res += "obese "
-    print(res);
+  temp = data.split("\n")
+  res=""
+  t=[]
+  bmi = 0
+  for i in range(0,int(temp[0])):
+    t = temp[i+1].split(" ")
+    bmi = float(t[0]) / pow(float(t[1]),2)
+    if (bmi < 18.5):
+      res += "under "
+    elif ( bmi >= 18.5 and bmi < 25.0):
+      res += "normal "
+    elif ( bmi >=25.0 and bmi < 30.0):
+      res += "over "
+    else:
+      res += "obese "
+  print(res)
