74486f0940526188b4e6a53fbf3b42d65f7e4e0c
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-04-08 13:06:54 -0500

    Update 3Rr0r404a.py

--- challenges/codeabbey/002/3Rr0r404a.py
+++ challenges/codeabbey/002/3Rr0r404a.py
@@ -1,8 +1,8 @@
 def sumaLoop1(data):
-    temp = data.split("\n");
-    t=[];
-    n=0;
-    for i in range(0,int(temp[0])):
-        t = temp[1].split(" ");
-        n += int(t[i]);
-    print(n)
+  temp = data.split("\n");
+  t=[];
+  n=0;
+  for i in range(0,int(temp[0])):
+    t = temp[1].split(" ");
+    n += int(t[i]);
+  print(n);
