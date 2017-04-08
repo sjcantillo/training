d7495d010a2286c27480ad0ab965f015b6ae42f3
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-04-08 12:27:13 -0500

    Update 3Rr0r404a.py

--- challenges/codeabbey/021/3Rr0r404a.py
+++ challenges/codeabbey/021/3Rr0r404a.py
@@ -1,15 +1,15 @@
 def arrayCounters(data):
-        temp = data.split("\n");
-        limits = temp[0].split(" ");
-        datos = temp[1].split(" ");
-        counter = [];
-        res = "";
-        index=0;
-        for i in range(0,int(limits[1])):
-                counter.append(0);
-        for x in range(0,int(limits[0])):
-                index=int(datos[x])-1;
-                counter[index] += 1;
-        for p in counter:
-                res += str(p) + " ";
-        print(res);
+  temp = data.split("\n");
+  limits = temp[0].split(" ");
+  datos = temp[1].split(" ");
+  counter = [];
+  res = "";
+  index=0;
+  for i in range(0,int(limits[1])):
+    counter.append(0);
+  for x in range(0,int(limits[0])):
+    index=int(datos[x])-1;
+    counter[index] += 1;
+  for p in counter:
+    res += str(p) + " ";
+  print(res);
