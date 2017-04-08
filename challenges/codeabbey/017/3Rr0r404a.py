e1b93f12d6d91ace5e371e398bcccc9f8aa69839
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-04-08 12:35:57 -0500

    Update 3Rr0r404a.py

--- challenges/codeabbey/017/3Rr0r404a.py
+++ challenges/codeabbey/017/3Rr0r404a.py
@@ -1,6 +1,6 @@
 def arrayChecksum(data):
-        temp = data.split("\n");
-        res = 0;
-        for num in temp[1].split(" "):
-                res = ((res + int(num))*113) % 10000007;
-        print(res);
+  temp = data.split("\n");
+  res = 0;
+  for num in temp[1].split(" "):
+    res = ((res + int(num))*113) % 10000007;
+  print(res);
