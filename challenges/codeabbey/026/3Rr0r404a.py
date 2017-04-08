6c17d25b09b8442e67972c4d7c2a929b90bbfa4b
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-04-08 12:21:05 -0500

    Update 3Rr0r404a.py

--- challenges/codeabbey/026/3Rr0r404a.py
+++ challenges/codeabbey/026/3Rr0r404a.py
@@ -1,20 +1,20 @@
 def gcd(data):
-        temp = data.split("\n");
-        t = [];
-        lcm = 0;
-        res = "";
-        for i in range(0,int(temp[0])):
-                t = temp[i+1].split(" ");
-                if int(t[0]) > int(t[1]):
-                        a = int(t[0]);
-                        b = int(t[1]);
-                else:
-                        a = int(t[1]);
-                        b = int(t[0]);
-                while(b != 0):
-                        tmp = b;
-                        b = a % b;
-                        a = tmp;
-                lcm = (int(t[0]) * int(t[1])) // a;
-                res += "(" + str(a) + " " + str(lcm) + ") ";
-        print(res);
+  temp = data.split("\n");
+  t = [];
+  lcm = 0;
+  res = "";
+  for i in range(0,int(temp[0])):
+    t = temp[i+1].split(" ");
+    if int(t[0]) > int(t[1]):
+      a = int(t[0]);
+      b = int(t[1]);
+    else:
+      a = int(t[1]);
+      b = int(t[0]);
+    while(b != 0):
+      tmp = b;
+      b = a % b;
+      a = tmp;
+    lcm = (int(t[0]) * int(t[1])) // a;
+    res += "(" + str(a) + " " + str(lcm) + ") ";
+  print(res);
