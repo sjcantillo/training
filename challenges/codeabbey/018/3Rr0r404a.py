f16779e3e697e8eddae1a0837866e8e00ece53ac
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-04-08 12:34:35 -0500

    Update 3Rr0r404a.py

--- challenges/codeabbey/018/3Rr0r404a.py
+++ challenges/codeabbey/018/3Rr0r404a.py
@@ -1,12 +1,12 @@
 def squareRoot(data):
-        temp = data.split("\n");
-        res = "";
-        r = 1;
-        cases = [];
-        for i in range(0,int(temp[0])):
-                cases = temp[i+1].split(" ");
-                r = 1;
-                for x in range(0,int(cases[1])):
-                        r = (r + (int(cases[0])/r))/2;
-                res += str(round(r,7)) + " ";
-        print(res);
+  temp = data.split("\n");
+  res = "";
+  r = 1;
+  cases = [];
+  for i in range(0,int(temp[0])):
+    cases = temp[i+1].split(" ");
+    r = 1;
+    for x in range(0,int(cases[1])):
+      r = (r + (int(cases[0])/r))/2;
+    res += str(round(r,7)) + " ";
+  print(res);
