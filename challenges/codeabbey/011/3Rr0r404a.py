6ba2d550609a2848512ca1cb95294aae1425a683
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-04-08 12:54:29 -0500

    Update 3Rr0r404a.py

--- challenges/codeabbey/011/3Rr0r404a.py
+++ challenges/codeabbey/011/3Rr0r404a.py
@@ -1,14 +1,14 @@
 def sumaDigitLoop(data):
-        temp = data.split("\n");
-        res = "";
-        t=[];
-        n=0;
-        r=0
-        for i in range(0,int(temp[0])):
-                t = temp[i+1].split(" ");
-                n = (int(t[0]) * int(t[1])) + int(t[2]);
-                for i in str(n):
-                        r +=  int(i);                        
-                res += str(r) + " ";
-                r = 0;
-        print(res);
+  temp = data.split("\n");
+  res = "";
+  t=[];
+  n=0;
+  r=0;
+  for i in range(0,int(temp[0])):
+    t = temp[i+1].split(" ");
+    n = (int(t[0]) * int(t[1])) + int(t[2]);
+    for i in str(n):
+      r +=  int(i);
+    res += str(r) + " ";
+    r = 0;
+  print(res);
