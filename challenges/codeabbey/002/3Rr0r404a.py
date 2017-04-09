cada6b2745f6a63e1093db169285b6952490679b
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-04-09 17:40:26 -0500

    Update 3Rr0r404a.py

--- challenges/codeabbey/002/3Rr0r404a.py
+++ challenges/codeabbey/002/3Rr0r404a.py
@@ -1,8 +1,9 @@
+"""Suma de numeros en ciclo"""
 def sumaLoop1(data):
-  temp = data.split("\n");
-  t=[];
-  n=0;
+  temp = data.split("\n")
+  t=[]
+  n=0
   for i in range(0,int(temp[0])):
-    t = temp[1].split(" ");
-    n += int(t[i]);
-  print(n);
+    t = temp[1].split(" ")
+    n += int(t[i])
+  print(n)
