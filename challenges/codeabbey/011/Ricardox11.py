f31bf1699750aa862cc956eeb36c9121cb18866a
Ricardo <metrpg@gmail.com>
2017-05-02 22:54:30 -0500

    Update Ricardox11.py
    mod3

--- challenges/codeabbey/011/Ricardox11.py
+++ challenges/codeabbey/011/Ricardox11.py
@@ -1,24 +1,24 @@
-n = 0
-resultadot = ""
-n = int(raw_input("Por favor digitar N: ")) # lee N valores a procesar
+ncon = 0
+resadot = ""
+ncon = int(raw_input("Por favor digitar N: "))
 
-for y in range(n):
-  valores = raw_input("Dato %i:" % y) # captura valores
-  valores = valores.split(" ") # separa en cadena
-  valor1 = int(valores[0]) # lee cada valor
-  valor2 = int(valores[1])
-  valor3 = int(valores[2])
+for y in range(ncon):
+    valores = raw_input("Dato %i:" % y)
+    valores = valores.split(" ")
+    valor1 = int(valores[0])
+    valor2 = int(valores[1])
+    valor3 = int(valores[2])
 
-  temporaldiv = 0
-  resultado = 0
-  temporal = (valor1 * valor2) + valor3 # suma valores
-  nvalor = len(str(temporal)) # longitud de valores a sumar
+    temporaldiv = 0
+    resultado = 0
+    temporal = (valor1 * valor2) + valor3
+    nvalor = len(str(temporal))
 
-  for i in range(nvalor):
-     temporaldiv = (temporal % 10) # usa modo para separar
-     temporal = (temporal / 10) # calcula siguiente numero
-     resultado = resultado + temporaldiv # suma cada valor
+    for i in range(nvalor):
+       temporaldiv = (temporal % 10)
+       temporal = (temporal / 10)
+       resultado = resultado + temporaldiv
 
-  resultadot = resultadot + " " +  str(resultado) # concatena resultados
+    resadot = resadot + " " +  str(resultado)
 
-print  resultadot.strip() # imprime resultado total
+print resadot.strip()
