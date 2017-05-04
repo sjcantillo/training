32a1c72da918e258eec2782b8f0cdb1c2484cd73
Ricardo <metrpg@gmail.com>
2017-05-03 21:04:15 -0500

    mod4
    mod4

--- challenges/codeabbey/011/Ricardox11.py
+++ challenges/codeabbey/011/Ricardox11.py
@@ -1,24 +1,26 @@
-ncon = 0
-resadot = ""
-ncon = int(raw_input("Por favor digitar N: "))
+# sumadigitos.py
+# sumar entrada de datos digitos
+nconta = 0
+resulat = ""
+nconta = int(raw_input("Por favor digitar N: "))  # lee N valores a procesar
 
-for y in range(ncon):
-    valores = raw_input("Dato %i:" % y)
-    valores = valores.split(" ")
-    valor1 = int(valores[0])
+for y in range(nconta):
+    valores = raw_input("Dato %i:" % y)  # captura valores
+    valores = valores.split(" ")  # separa en cadena
+    valor1 = int(valores[0])  # lee cada valor
     valor2 = int(valores[1])
     valor3 = int(valores[2])
 
     temporaldiv = 0
     resultado = 0
-    temporal = (valor1 * valor2) + valor3
-    nvalor = len(str(temporal))
+    temporal = (valor1 * valor2) + valor3   # suma valores
+    nvalor = len(str(temporal))  # longitud de valores a sumar
 
     for i in range(nvalor):
-       temporaldiv = (temporal % 10)
-       temporal = (temporal / 10)
-       resultado = resultado + temporaldiv
+        temporaldiv = (temporal % 10)  # usa modo para separar
+        temporal = (temporal / 10)  # calcula siguiente numero
+        resultado = resultado + temporaldiv  # suma cada valor
 
-    resadot = resadot + " " +  str(resultado)
+    resulat = resulat + " " + str(resultado)  # concatena resultados
 
-print resadot.strip()
+print resulat.strip()  # imprime resultado total
