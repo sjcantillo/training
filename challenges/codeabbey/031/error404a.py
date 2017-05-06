5c1cf0c301844bcdbd965eddbf34c00af89a7c72
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-05-06 12:08:09 -0500

    3Rr0r404a.py
    Se solucionan problemas de identación

---
+++ challenges/codeabbey/031/error404a.py
@@ -0,0 +1,23 @@
+"""This function cuts the n charsfrom the begining of string
+   and put it in the end.
+   input data:
+   2
+   3 forwhomthebelltolls
+   -6 verycomplexnumber"""
+
+def rotatek(data):
+    """Rotate n character to the end of string"""
+    temp = data.split("\n")
+    res = ""
+    ltmp = []
+    ptemp = ""
+    lont = 0
+    nuCut = 0
+    word = ""
+    for i in range(0, int(temp[0])):
+        ltmp = temp[i+1].split(" ")
+        nuCut = int(t[0])
+        word = ltmp[1]
+        lont = len(word)
+        res += word[nuCut:lont] + word[0:nuCut] + " "
+  print res
