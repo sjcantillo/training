fb39578d4bdc3aed69a640dc579e4063189a23a5
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-05-06 13:07:25 -0500

    3Rr0r404a.py
    Se solucionan problemas de identación y nombramiento de variables

--- challenges/codeabbey/029/error404a.py
+++ challenges/codeabbey/029/error404a.py
@@ -9,18 +9,18 @@ def sortwithindex(data):
     res = ""
     for i in range(0, repets):
         temp[i] = [temp[i],(i+1)]
-    disorderFlg = True
-    swapFlg = False
-    while disorderFlg:
-        swapFlg = False
+    disorderflg = True
+    swapflg = False
+    while disorderflg:
+        swapflg = False
         for i in range(0, (repets-1)):
-          if int(temp[i+1][0]) < int(temp [i][0]):
-              swapFlg = True
-              t = temp[i]
-              temp[i] = temp[i+1]
-              temp[i+1] = t
-        if not(swapFlg):
-            disorderFlg = False
+            if int(temp[i+1][0]) < int(temp [i][0]):
+                swapflg = True
+                t = temp[i]
+                temp[i] = temp[i+1]
+                temp[i+1] = t
+        if not(swapflg):
+            disorderflg = False
     for n in temp:
         res += str(n[1]) + " "
     print res
