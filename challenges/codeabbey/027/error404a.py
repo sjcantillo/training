bbc946c1133e0e1f13ef66780127c83783d2165f
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-05-06 13:43:09 -0500

    3Rr0r404a.py
    Se solucionan problemas de paréntesis innecesarios

--- challenges/codeabbey/027/error404a.py
+++ challenges/codeabbey/027/error404a.py
@@ -3,6 +3,7 @@ how many swaps and itrerations it was needed"""
 
 
 def bubblesort(data):
+    """Sort an array in non-decreasing order."""
     temp = data.split("\n")
     repets = int(temp[0])
     temp = temp[1].split(" ")
@@ -10,16 +11,16 @@ def bubblesort(data):
     swapflg = False
     contswap = 0
     contpasses = 0
-    while(disorderflg):
+    while disorderflg:
         swapflg = False
         for i in range(0, (repets-1)):
-          if(int(temp[i+1]) < int(temp [i])):
-            swapflg = True
-            contswap += 1
-            t = temp[i]
-            temp[i] = temp[i+1]
-            temp[i+1] = t
+            if int(temp[i+1]) < int(temp[i]):
+                swapflg = True
+                contswap += 1
+                tmp = temp[i]
+                temp[i] = temp[i+1]
+                temp[i+1] = tmp
         contpasses += 1
-        if not(swapflg):
+        if not swapflg:
             disorderflg = False
     print str(contpasses) + " " + str(contswap)
