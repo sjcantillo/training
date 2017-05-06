137889ef6513d8f0be4a58f982a76ba78606fbab
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-05-06 12:58:35 -0500

    3Rr0r404a.py
    Se corrigen problemas de identación y nombramiento

---
+++ challenges/codeabbey/029/error404a.py
@@ -0,0 +1,26 @@
+"""This function order an array of numbers
+and return the original index"""
+
+
+def sortwithindex(data):
+    temp = data.split("\n")
+    repets = int(temp[0])
+    temp = temp[1].split(" ")
+    res = ""
+    for i in range(0, repets):
+        temp[i] = [temp[i],(i+1)]
+    disorderFlg = True
+    swapFlg = False
+    while disorderFlg:
+        swapFlg = False
+        for i in range(0, (repets-1)):
+          if int(temp[i+1][0]) < int(temp [i][0]):
+              swapFlg = True
+              t = temp[i]
+              temp[i] = temp[i+1]
+              temp[i+1] = t
+        if not(swapFlg):
+            disorderFlg = False
+    for n in temp:
+        res += str(n[1]) + " "
+    print res
