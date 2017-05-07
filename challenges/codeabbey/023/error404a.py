6960e6d227acf97905300266249138c2e1047087
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-05-07 00:46:49 -0500

    3Rr0r404a.py
    Se modifico la función y el nombre del archivo por errores reportados por travis

--- challenges/codeabbey/023/error404a.py
+++ challenges/codeabbey/023/error404a.py
@@ -3,20 +3,18 @@
  of the array"""
 
 
-def bubbleInArray(data):
+def bubbleinarray(data):
     """Perform the sort"""
     temp = data.split(" ")
     temp.pop(len(temp) - 1)
-    disorderflg = True
     contswap = 0
     arrayhash = 0
     for i in range(0, (len(temp) - 1)):
-      if int(temp[i+1]) < int(temp[i]):
-          swapflg = True
-          contswap += 1
-          tmp = temp[i]
-          temp[i] = temp[i+1]
-          temp[i+1] = tmp
+        if int(temp[i+1]) < int(temp[i]):
+            contswap += 1
+            tmp = temp[i]
+            temp[i] = temp[i+1]
+            temp[i+1] = tmp
     for num in temp:
         arrayhash = ((arrayhash + int(num)) * 113) % 10000007
     print str(contswap) + " " + str(arrayhash)
