b28b22d725186fae9a508aa98bb2dc9324b7d674
Ricardo <metrpg@gmail.com>
2017-05-07 16:41:06 -0500

    mod 1

--- challenges/codeabbey/013/Ricardox11.py
+++ challenges/codeabbey/013/Ricardox11.py
@@ -3,12 +3,12 @@
 
 RANGC = int(raw_input("Por favor digitar N: "))  # lee N valores a procesar
 RESUL2 = ""
-valores = raw_input("Dato: ")  # captura valores1
-valores = valores.split(" ")  # separa en cadena
+VALORES = raw_input("Dato: ")  # captura valores1
+VALORES = VALORES.split(" ")  # separa en cadena
 
 for y in range(RANGC):
     
-    temporal = int(valores[y])  # lee cada valor
+    temporal = int(VALORES[y])  # lee cada valor
     temporaldiv = 0
     resultado = 0
     nvalor = len(str(temporal))  # longitud de valores a sumar
@@ -16,9 +16,9 @@ for y in range(RANGC):
     for i in range(nvalor):
         temporaldiv = (temporal % 10)  # usa modo para separar
         temporal = (temporal / 10)  # calcula siguiente numero
-        resultado = resultado + (temporaldiv * (nvalor1))  # suma cada valor y multiplica posicion
+        resultado = resultado + (temporaldiv * (nvalor1))  # sum val mult pos
         nvalor1 = nvalor1 - 1
         
     RESUL2 = RESUL2 + " " + str(resultado)  # concatena resultados
 
-print RESUL2.strip() # imprime resultado total
+print RESUL2.strip() #  imprime resultado totaL
