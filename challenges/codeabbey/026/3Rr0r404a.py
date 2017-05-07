cdb5307e6c3c3caface2b60a2d61d0e4f2690711
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-05-06 21:11:53 -0500

    3Rr0r404a.py
    Se corrigen errores reportados por travis en identación y nombramiento de variables

--- challenges/codeabbey/026/3Rr0r404a.py
+++ challenges/codeabbey/026/3Rr0r404a.py
@@ -1,20 +1,26 @@
+"""This function find the greatest common divisor
+and Least Common Multiple"""
+
+
+
 def gcd(data):
-  temp = data.split("\n");
-  t = [];
-  lcm = 0;
-  res = "";
-  for i in range(0,int(temp[0])):
-    t = temp[i+1].split(" ");
-    if int(t[0]) > int(t[1]):
-      a = int(t[0]);
-      b = int(t[1]);
-    else:
-      a = int(t[1]);
-      b = int(t[0]);
-    while(b != 0):
-      tmp = b;
-      b = a % b;
-      a = tmp;
-    lcm = (int(t[0]) * int(t[1])) // a;
-    res += "(" + str(a) + " " + str(lcm) + ") ";
-  print(res);
+    """Find the gcd and lcm"""
+    temp = data.split("\n")
+    ltmp = []
+    lcm = 0
+    res = ""
+    for i in range(0, int(temp[0])):
+        ltmp = temp[i+1].split(" ")
+        if int(ltmp[0]) > int(ltmp[1]):
+            vara = int(ltmp[0])
+            varb = int(ltmp[1])
+        else:
+            vara = int(ltmp[1])
+            varb = int(ltmp[0])
+        while b != 0:
+            tmp = varb
+            varb = vara % varb
+            vara = tmp;
+        lcm = (int(ltmp[0]) * int(ltmp[1])) // vara
+        res += "(" + str(vara) + " " + str(lcm) + ") "
+    print res
