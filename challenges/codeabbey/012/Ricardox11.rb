82564567640b168f1cd957ace1567c52154bb089
Ricardo <metrpg@gmail.com>
2017-05-07 19:34:57 -0500

    mod 4
    mod 4

--- challenges/codeabbey/012/Ricardox11.rb
+++ challenges/codeabbey/012/Ricardox11.rb
@@ -7,8 +7,7 @@ n = gets.chomp.to_i
   puts "Por favor ingresar dato #{x}"
   time = gets.chomp # captura datos
   fecha = time.split(' ') # convierte a arreglo
-
-  dia1 = fecha[0].to_i # separa datos
+  dia1 = fecha[0].to_i
   hora1 = fecha[1].to_i
   min1 = fecha[2].to_i
   seg1 = fecha[3].to_i
@@ -16,18 +15,13 @@ n = gets.chomp.to_i
   hora2 = fecha[5].to_i
   min2 = fecha[6].to_i
   seg2 = fecha[7].to_i
-
   fecha_seg1 = (dia1 * 864_00) + (hora1 * 3_600) + (min1 * 60) + seg1
   fecha_seg2 = (dia2 * 864_00) + (hora2 * 3_600) + (min2 * 60) + seg2
-
   fecha_dif_seg = (fecha_seg2 - fecha_seg1) # calcula diferencia
-
   dian = (fecha_dif_seg / 86_400) # calcula dia
   horan = ((fecha_dif_seg % 86_400) / 3_600) # calcula hora
   minn  = (((fecha_dif_seg % 86_400) % 3_600) / 60) # calcula minutos
   segn = (((fecha_dif_seg % 86_400) % 3_600) % 60) # calcula segundos
-
   respuesta += "(#{dian} #{horan} #{minn} #{segn}) "
 end
-
 puts respuesta
