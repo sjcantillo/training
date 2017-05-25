96105e7dcec9f9fc130a3acd2204dab859b8de45
Juan Aguirre <juan_aguirre02@hotmail.com>
2017-05-24 21:38:21 -0500

    fixing mixed tabs

--- file_search.py
+++ file_search.py
@@ -14,14 +14,14 @@ def find_sources(ext, mode, directory='challenges/'):
     """Fn to find sources based on an ext and under the default dir, challenges.
 
     Args:
-	    ext       (string): Extention of desired files.
-		mode      (int)   : 0 for any file name with desired ext. 1 for OTHERS.
-		directory (string): Default dir 'challenges/'.
+        ext       (string): Extention of desired files.
+        mode      (int)   : 0 for any file name with desired ext. 1 for OTHERS.
+        directory (string): Default dir 'challenges/'.
 
-	Returns:
-	    List: matches[]. Contains list of file names with desired ext.
+    Returns:
+        List: matches[]. Contains list of file names with desired ext.
 
-	"""
+    """
 
     matches = []
 #   iterate through dirs and append to matches
@@ -35,4 +35,3 @@ def find_sources(ext, mode, directory='challenges/'):
                 matches.append(os.path.join(root, filename))
 
     return matches
-    #return dir_matches
