0b3763ed87b903664053b30924bb1f609cac6c55
Juan Aguirre <juan_aguirre02@hotmail.com>
2017-05-26 13:00:44 -0500

    re run travis test

--- site_scons/others_builder.py
+++ site_scons/others_builder.py
@@ -45,7 +45,7 @@ def build_others(target, source, env):
         stat_code = 0
         ur_len = len(url)
         if ur_len > 0:
-            # Prepend protocol if missing
+            # Add protocol if missing
             if not url.startswith("http"):
                 url = 'https://' + url
             try:
