4c698b9b4812519f74cacc29bb44a2f2ec5fc997
Juan Aguirre <juan_aguirre02@hotmail.com>
2017-05-31 10:45:27 -0500

    ident fix

--- site_scons/others_builder.py
+++ site_scons/others_builder.py
@@ -49,7 +49,8 @@ def build_others(target, source, env):
         'Accept': 'text/html,application/xhtml+xml,'
                   'application/xml;q=0.9,*/*;q=0.8',
         'User-Agent': 'Mozilla/5.0 (Windows NT 5.1;rv:10.0.1)'
-                      'Gecko/20100101 Firefox/10.0.1', }
+                      'Gecko/20100101 Firefox/10.0.1'
+	}
     # Iterate through urls
     for url in cont_arr:
         stat_code = 0
