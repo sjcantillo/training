7ab8756a67ab330a2ffcaad6aa7c06d47fecf86d
jrestrepoatfluid <jrestrepo@fluid.la>
2017-05-31 17:35:39 -0500

    Update jrestrepo.py

--- challenges/codeeval/medium/017/jrestrepo.py
+++ challenges/codeeval/medium/017/jrestrepo.py
@@ -1,25 +1,29 @@
-import sys
+#!/usr/bin/python3
+"""Codeval challenge17"""
 
 def get_tests():
+    """Get data"""
     cases = []
-    with open(sys.argv[1], "r") as f:
-        for i in f:
+    with open(sys.argv[1], "r") as file:
+        for i in file:
             cases.append(i.strip())
     return cases
 
 def suma(lista):
-    data = list(map(int,lista.split(',')))
-    max = min(data)
+    """Get max sum"""
+    data = list(map(int, lista.split(',')))
+    maximum = min(data)
     res = 0
     tam = len(data)
     for indx in range(tam):
-        for indy in range(indx+1,tam):
+        for indy in range(indx+1, tam):
             res = (sum(data[indx:indy]))
-            if res > max:
-                max = res
-    print (max)
+            if res > maximum:
+                maximum = res
+    print(max)
 
 def main():
+    """main function"""
     tests = get_tests()
     for integer in tests:
         suma(integer)
