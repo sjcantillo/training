9235e5ee987550fcf7b969ef31f25c9acb79c30e
Juan Aguirre <juan_aguirre02@hotmail.com>
2017-06-18 07:22:03 -0500

    # Commit title: Implementing language builders
    # Files changed:
    	.travis.yml : removed checkstyle install
    	site_scons/linter_module/java_linters.py : Implement checkstyle linter using .jar and embedded sun style checks.
    	site_scons/linter_module/resources/google_checks.xml : Deleted - unused.
    
    # TODO
    implement other linters
    refactor linter exec
    refactor command handled
    Organize travis config using jobs

--- site_scons/linter_module/java_linters.py
+++ site_scons/linter_module/java_linters.py
@@ -26,8 +26,10 @@ def java_bld(fname):
     out_msg = "java lint - checkstyle "
     # Set command
     lint_cmd = ["java"]
-    lint_cmd.append("-jar site_scons/linter_module/resources/checkstyle-7.8.1-all.jar")
-    lint_cmd.append("-c /sun_checks.xml")
+    lint_cmd.append("-jar")
+    lint_cmd.append("site_scons/linter_module/resources/checkstyle-7.8.1-all.jar")
+    lint_cmd.append("-c")
+    lint_cmd.append("/sun_checks.xml")
     out_lint = command_handler.run_cmd(lint_cmd, fname)
     # Prep result
     build_result.append(out_lint)
