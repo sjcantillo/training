32304ad20087fc2eb1f89dfd7bad38e566bb28ee
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-07-06 22:20:34 -0500

    3Rr0r404a.py
    Se corrige identación

--- challenges/codeabbey/006/error404.py
+++ challenges/codeabbey/006/error404.py
@@ -7,8 +7,8 @@ def divround(data):
     res = ""
     tlist = []
     nudiv = 0
-    for i in range(0,int(temp[0])):
-      tlist = temp[i+1].split(" ")
-      nudiv = round(int(tlist[0]) / int(tlist[1]))
-      res += str(nudiv) + " "
+    for i in range(0, int(temp[0])):
+        tlist = temp[i+1].split(" ")
+        nudiv = round(int(tlist[0]) / int(tlist[1]))
+        res += str(nudiv) + " "
     print res
