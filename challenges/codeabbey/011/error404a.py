146e598ef45cc4ec4afbc2d35c4f07cd6a284be2
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-07-06 20:45:51 -0500

    3Rr0r404a.py
    Se modifica la identacion

--- challenges/codeabbey/011/error404a.py
+++ challenges/codeabbey/011/error404a.py
@@ -3,17 +3,17 @@ of the result of multiplaying a * b + c"""
 
 
 def sumadigitloop(data):
-  """Calculate de digit sum"""
-  temp = data.split("\n")
-  res = ""
-  tlist = []
-  opres = 0
-  digsum = 0
-  for i in range(0, int(temp[0])):
-    tlist = temp[i+1].split(" ")
-    opres = (int(tlist[0]) * int(tlist[1])) + int(tlist[2])
-    for i in str(opres):
-      digsum +=  int(i)
-    res += str(digsum) + " "
+    """Calculate de digit sum"""
+    temp = data.split("\n")
+    res = ""
+    tlist = []
+    opres = 0
     digsum = 0
-  print res
+    for i in range(0, int(temp[0])):
+        tlist = temp[i+1].split(" ")
+        opres = (int(tlist[0]) * int(tlist[1])) + int(tlist[2])
+        for i in str(opres):
+            digsum += int(i)
+        res += str(digsum) + " "
+        digsum = 0
+    print res
