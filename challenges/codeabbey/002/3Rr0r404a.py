42bc09207331b48e508f93d67ba620dc63fb7614
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-07-06 22:46:57 -0500

    3Rr0r404a.py
    Se corrige la sintaxis de la solución para que cumpla las reglas de travis

--- challenges/codeabbey/002/3Rr0r404a.py
+++
@@ -1,9 +0,0 @@
-"""Suma de numeros en ciclo"""
-def sumaLoop1(data):
-  temp = data.split("\n")
-  t=[]
-  n=0
-  for i in range(0,int(temp[0])):
-    t = temp[1].split(" ")
-    n += int(t[i])
-  print(n)
