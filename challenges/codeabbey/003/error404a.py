9424eb569fa7bd51f3144e9efe7fa6f1873f720b
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-07-06 22:41:49 -0500

    3Rr0r404a.py
    Se corrige la sintaxis de la solución para que cumpla con las reglas de travis

---
+++ challenges/codeabbey/003/error404a.py
@@ -0,0 +1,14 @@
+"""This function sum the number in array of length 2"""
+
+
+def sumaloop(data):
+  """Sum tow number in an array"""
+  temp = data.split("\n")
+  res = ""
+  tlist = []
+  nusum = 0
+  for i in range(0, int(temp[0])):
+    tlist = temp[i+1].split(" ")
+    nusum = int(tlist[0]) + int(tlist[1])
+    res += str(nusum) + " "
+  print res
