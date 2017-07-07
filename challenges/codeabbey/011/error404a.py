f9ea561d83dbbe5705b26e41bee4b1f9d1955e18
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-07-06 20:35:48 -0500

    3Rr0r404a.py
    Se corrige la sintaxis de la solución para que cumpla con las reglas de travis

---
+++ challenges/codeabbey/011/error404a.py
@@ -0,0 +1,19 @@
+"""This function calculate the sum of the digits
+of the result of multiplaying a * b + c"""
+
+
+def sumadigitloop(data):
+  """Calculate de digit sum"""
+  temp = data.split("\n")
+  res = ""
+  tlist = []
+  opres = 0
+  digsum = 0
+  for i in range(0, int(temp[0])):
+    tlist = temp[i+1].split(" ")
+    opres = (int(tlist[0]) * int(tlist[1])) + int(tlist[2])
+    for i in str(opres):
+      digsum +=  int(i)
+    res += str(digsum) + " "
+    digsum = 0
+  print res
