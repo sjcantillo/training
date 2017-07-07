440b637a374286dde6e01c833073faf9342c50c1
3Rr0r404a <ing.josealejandro@hotmail.com>
2017-07-06 20:43:31 -0500

    3Rr0r404a.py
    Se modifica el número de variables utilizada

--- challenges/codeabbey/012/error404a.py
+++ challenges/codeabbey/012/error404a.py
@@ -15,16 +15,8 @@ def moduletimediference(data):
     nuhour1 = 0
     numin1 = 0
     tdif1 = 0
-    nuday2 = 0
-    nuhour2 = 0
-    numin2 = 0
     tdif2 = 0
-    timeDif = 0
     res = ""
-    days = 0
-    hours = 0
-    minutes = 0
-    segs = 0
     daytime = 60 * 60 * 24
     hourtime = 60 * 60
     for i in range(0, int(temp[0])):
@@ -33,15 +25,14 @@ def moduletimediference(data):
         nuhour1 = int(tlist[5]) * hourtime
         numin1 = int(tlist[6]) * 60
         tdif1 = nuday1 + nuhour1 + numin1 + int(tlist[7])
-        nuday2 = int(tlist[0]) * daytime
-        nuhour2 = int(tlist[1]) * hourtime
-        numin2 = int(tlist[2]) * 60
-        tdif2 = nuday2 + nuhour2 + numin2 + int(tlist[3])
-        timeDif = tdif1 - tdif2 
-        days = timeDif // daytime
-        hours = (timeDif % daytime) // hourtime
-        minutes = (timeDif % hourtime) // 60
-        segs = timeDif % 60
-        res += "(" + str(days) + " " + str(hours) 
-        res += " " + str(minutes) + " " + str(segs) + ") "
+        nuday1 = int(tlist[0]) * daytime
+        nuhour1 = int(tlist[1]) * hourtime
+        numin1 = int(tlist[2]) * 60
+        tdif2 = nuday1 + nuhour1 + numin1 + int(tlist[3])
+        tdif1 = tdif1 - tdif2 
+        nuday1 = tdif1 // daytime
+        nuhour1 = (tdif1 % daytime) // hourtime
+        numin1 = (tdif1 % hourtime) // 60
+        res += "(" + str(nuday1) + " " + str(nuhour1) 
+        res += " " + str(numin1) + " " + str(tdif1 % 60) + ") "
     print res
