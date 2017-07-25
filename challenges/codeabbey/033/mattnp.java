d2d418540071c777e70741a009260c16fd73eb27
Mateo Noreña <npmatt23@gmail.com>
2017-07-24 23:01:22 -0500

    Update mattnp.java

--- challenges/codeabbey/033/mattnp.java
+++ challenges/codeabbey/033/mattnp.java
@@ -2,19 +2,15 @@ package solution;
 
 import java.util.Scanner;
 
-public class Solution
-{
-    public static void main(String[] args)
-    {
+public class Solution {
+    public static void main(String[] args) {
         final int p = 46;
         final int h = 128;
         Scanner in = new Scanner(System.in);
         String message = "";
         int n;
-        while ((n = in.nextInt()) != p)
-        {
-            if (Integer.bitCount(n) % 2 == 0)
-            {
+        while ((n = in.nextInt()) != p) {
+            if (Integer.bitCount(n) % 2 == 0) {
               if (n >= h) {
                 n -= h;
               }
