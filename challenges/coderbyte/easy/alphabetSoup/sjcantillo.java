8de730cd9d41da046665a5a213bf8597ee9e1960
sjcantillo <sjcantillo@gmail.com>
2017-08-02 16:42:58 -0500

    Corrección sjcantillo.java

--- challenges/coderbyte/easy/alphabetSoup/sjcantillo.java
+++ challenges/coderbyte/easy/alphabetSoup/sjcantillo.java
@@ -1,16 +1,12 @@
 package com.fluid;
 import java.util.Arrays;
 import java.util.Scanner;
-
 public class Main {
-	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print(alphabetSoup(scanner.nextLine()));
-	}
-	
+	}	
 	public static String alphabetSoup(String string) {
-
 		char cadenaCaractares[] = string.toCharArray();
 		Arrays.sort(cadenaCaractares);
 		StringBuilder stringBuilder = new StringBuilder();
@@ -18,8 +14,5 @@ public class Main {
 			stringBuilder.append(caracter);
 		}
 		return stringBuilder.toString();
-
 	}
-
-
 }
