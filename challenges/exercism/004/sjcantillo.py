eec55e87c9b025a116a641c2bb2c64f5b98f162d
sjcantillo <sjcantillo@gmail.com>
2017-08-03 20:47:30 -0500

    Se añade el archivo .py para la solución del reto 5

---
+++ challenges/exercism/004/sjcantillo.py
@@ -0,0 +1,20 @@
+def raindrops(number):
+  divider = 2
+  numberPrincipal=number
+  raindropString= ""
+  dictionary = {3:"Pling", 5:"Plang", 7:"Plong"}
+  while number !=  1:
+    if number%divider == 0:
+      number /= divider
+    else:
+     divider += 1
+     if divider in dictionary:
+       raindropString += dictionary[divider]
+       
+  if len(raindropString) == 0 :
+    raindropString=str(numberPrincipal)
+    
+  return raindropString
+
+var = int(input())      
+print(raindrops(var))
