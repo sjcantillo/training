a28cc01edafcd2c7366eb96cd4543cdebe9cf7a3
sjcantillo <sjcantillo@gmail.com>
2017-08-04 06:28:36 -0500

    Se añade el archivo .py para la solución del reto 6

--- challenges/exercism/012/sjcantillo.py
+++ challenges/exercism/012/sjcantillo.py
@@ -2,10 +2,10 @@ def ispangram(sentence):
     sentence = sentence.lower()
     count_letter = 0
     while len(sentence) != 0:
-        letter=sentence[0]
+        letter = sentence[0]
         if ord(letter) >= 97 and ord(letter) <= 122:
             count_letter += 1
-        sentence = sentence.replace(letter,"")
+        sentence = sentence.replace(letter, "")
     if count_letter == 26:
         return True
     return False
