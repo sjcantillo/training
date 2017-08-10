5f86ff493df899195ee5be1acc49d7b0aa06f4e2
sjcantillo <sjcantillo@gmail.com>
2017-08-10 12:31:02 -0500

    Corrección Fix CheckStyle

--- challenges/codeeval/hard/110/sjcantillo.java
+++ challenges/codeeval/hard/110/sjcantillo.java
@@ -16,77 +16,109 @@ import java.util.Map;
  */
 public final class Main {
     /** Alphabet Numbers. */
-    private Map<String, Integer> ALPHABET_NUMBERS = null;
+    private Map<String, Integer> alphabetNumbers = null;
+    /** Number. */
     private static final Integer NEGATIVE = -1;
+    /** Number. */
     private static final Integer ZERO = 0;
+    /** Number. */
     private static final Integer ONE = 1;
+    /** Number. */
     private static final Integer TWO = 2;
+    /** Number. */
     private static final Integer THREE = 3;
+    /** Number. */
     private static final Integer FOUR = 4;
+    /** Number. */
     private static final Integer FIVE = 5;
+    /** Number. */
     private static final Integer SIX = 6;
+    /** Number. */
     private static final Integer SEVEN = 7;
+    /** Number. */
     private static final Integer EIGHT = 8;
+    /** Number. */
     private static final Integer NINE = 9;
+    /** Number. */
     private static final Integer TEN = 10;
+    /** Number. */
     private static final Integer ELEVEN = 11;
+    /** Number. */
     private static final Integer TWELVE = 12;
+    /** Number. */
     private static final Integer THIRTEEN = 13;
+    /** Number. */
     private static final Integer FOURTEEN = 14;
+    /** Number. */
     private static final Integer FIFTEEN = 15;
+    /** Number. */
     private static final Integer SIXTEEN = 16;
+    /** Number. */
     private static final Integer SEVENTEEN = 17;
+    /** Number. */
     private static final Integer EIGHTEEN = 18;
+    /** Number. */
     private static final Integer NINETEEN = 19;
+    /** Number. */
     private static final Integer TWENTY = 20;
+    /** Number. */
     private static final Integer THIRTY = 30;
+    /** Number. */
     private static final Integer FORTY = 40;
+    /** Number. */
     private static final Integer FIFTY = 50;
+    /** Number. */
     private static final Integer SIXTY = 60;
+    /** Number. */
     private static final Integer SEVENTY = 70;
+    /** Number. */
     private static final Integer EIGHTY = 80;
+    /** Number. */
     private static final Integer NINETY = 90;
+    /** Number. */
     private static final Integer HUNDRED = 100;
+    /** Number. */
     private static final Integer THOUSAND = 1000;
+    /** Number. */
     private static final Integer MILLION = 1000000;
 
     /** Private constructor. */
     private Main() {
     }
-    static{
-        ALPHABET_NUMBERS = new HashMap<String, Integer>();
-    	ALPHABET_NUMBERS.put("negative", NEGATIVE);
-    	ALPHABET_NUMBERS.put("zero", ZERO);
-    	ALPHABET_NUMBERS.put("one", ONE);
-    	ALPHABET_NUMBERS.put("two", TWO);
-    	ALPHABET_NUMBERS.put("three", THREE);
-    	ALPHABET_NUMBERS.put("four", FOUR);
-    	ALPHABET_NUMBERS.put("five", FIVE);
-    	ALPHABET_NUMBERS.put("six", SIX);
-    	ALPHABET_NUMBERS.put("seven", SEVEN);
-    	ALPHABET_NUMBERS.put("eight", EIGHT);
-    	ALPHABET_NUMBERS.put("nine", NINE);
-    	ALPHABET_NUMBERS.put("ten", TEN);
-    	ALPHABET_NUMBERS.put("eleven", ELEVEN);
-    	ALPHABET_NUMBERS.put("twelve", TWELVE);
-    	ALPHABET_NUMBERS.put("thirteen", THIRTEEN);
-    	ALPHABET_NUMBERS.put("fourteen", FOURTEEN);
-    	ALPHABET_NUMBERS.put("fifteen", FIFTEEN);
-    	ALPHABET_NUMBERS.put("sixteen", SIXTEEN);
-    	ALPHABET_NUMBERS.put("seventeen", SEVENTEEN);
-    	ALPHABET_NUMBERS.put("eighteen", EIGHTEEN);
-    	ALPHABET_NUMBERS.put("nineteen", NINETEEN);
-    	ALPHABET_NUMBERS.put("twenty", TWENTY);
-    	ALPHABET_NUMBERS.put("thirty", THIRTY);
-    	ALPHABET_NUMBERS.put("forty", FORTY);
-    	ALPHABET_NUMBERS.put("fifty", FIFTY);
-    	ALPHABET_NUMBERS.put("sixty", SIXTY);
-    	ALPHABET_NUMBERS.put("seventy", SEVENTY);
-    	ALPHABET_NUMBERS.put("eighty", EIGHTY);
-    	ALPHABET_NUMBERS.put("ninety", NINETY);
-    	ALPHABET_NUMBERS.put("hundred", HUNDRED);
-    	ALPHABET_NUMBERS.put("thousand", THOUSAND);
-    	ALPHABET_NUMBERS.put("million", MILLION);
+    static {
+        alphabetNumbers = new HashMap<String, Integer>();
+        alphabetNumbers.put("negative", NEGATIVE);
+    	alphabetNumbers.put("zero", ZERO);
+    	alphabetNumbers.put("one", ONE);
+    	alphabetNumbers.put("two", TWO);
+    	alphabetNumbers.put("three", THREE);
+    	alphabetNumbers.put("four", FOUR);
+    	alphabetNumbers.put("five", FIVE);
+    	alphabetNumbers.put("six", SIX);
+    	alphabetNumbers.put("seven", SEVEN);
+    	alphabetNumbers.put("eight", EIGHT);
+    	alphabetNumbers.put("nine", NINE);
+    	alphabetNumbers.put("ten", TEN);
+    	alphabetNumbers.put("eleven", ELEVEN);
+    	alphabetNumbers.put("twelve", TWELVE);
+    	alphabetNumbers.put("thirteen", THIRTEEN);
+    	alphabetNumbers.put("fourteen", FOURTEEN);
+    	alphabetNumbers.put("fifteen", FIFTEEN);
+    	alphabetNumbers.put("sixteen", SIXTEEN);
+    	alphabetNumbers.put("seventeen", SEVENTEEN);
+    	alphabetNumbers.put("eighteen", EIGHTEEN);
+    	alphabetNumbers.put("nineteen", NINETEEN);
+    	alphabetNumbers.put("twenty", TWENTY);
+    	alphabetNumbers.put("thirty", THIRTY);
+    	alphabetNumbers.put("forty", FORTY);
+    	alphabetNumbers.put("fifty", FIFTY);
+    	alphabetNumbers.put("sixty", SIXTY);
+    	alphabetNumbers.put("seventy", SEVENTY);
+    	alphabetNumbers.put("eighty", EIGHTY);
+    	alphabetNumbers.put("ninety", NINETY);
+    	alphabetNumbers.put("hundred", HUNDRED);
+    	alphabetNumbers.put("thousand", THOUSAND);
+    	alphabetNumbers.put("million", MILLION);
     }
     /**
      * Metodo para transformar el texto en numero.
@@ -101,15 +133,15 @@ public final class Main {
     	Integer numberFinal = 0;
     	for (int i = numbers.length - 1; i >= 0; i--) {
     		String number = numbers[i].trim();
-    		if (ALPHABET_NUMBERS.containsKey(number)) {
-    			Integer numberConvert = ALPHABET_NUMBERS.get(number);
-    			if (Integer.toString(numberConvert).length() >= 3) {
+    		if (alphabetNumbers.containsKey(number)) {
+    			Integer numberConvert = alphabetNumbers.get(number);
+    			if (Integer.toString(numberConvert).length() >= THREE) {
     				numberAuxuliar = numberConvert;
     			} else if (numberAuxuliar > 0) {
     				numberAuxuliar *= numberConvert;
     				numberFinal += numberAuxuliar;
     				numberAuxuliar = 0;
-    			} else if (numberConvert <0) {
+    			} else if (numberConvert < 0) {
     				numberFinal *= numberConvert;
     			} else {
     				numberFinal += numberConvert;
@@ -136,7 +168,7 @@ public final class Main {
 			fileReader = new FileReader(pathFile);
 			bufferedReader = new BufferedReader(fileReader);
 			String currentLine;
-			while (( currentLine = bufferedReader.readLine() ) != null) {
+			while ((currentLine = bufferedReader.readLine()) != null) {
 				listLine.add(currentLine.trim());
 			}
 		} catch (FileNotFoundException e) {
@@ -156,10 +188,10 @@ public final class Main {
     public static void main(final String[] args) {
         java.util.Scanner scanner = new java.util.Scanner(System.in);
         String pathFile = scanner.nextLine();
-        ArrayList<String> listLine =readFile(pathFile);
+        ArrayList<String> listLine = readFile(pathFile);
         for (String line: listLine) {
         	Integer number = convertTextToNumber(line);
-        	if(number != null) {
+        	if (number != null) {
         		System.out.println(number);
         	} else {
         		System.out.println("Number invalid");
